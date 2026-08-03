72ab583b206a92d33802cd0834df2cb4556950bc
alanpv92 <alanpv92@gmail.com>
2026-08-03 18:26:21 +0530

    kafka setup

--- patient-service/src/main/java/com/projects/patientmanagement/patient_service/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/projects/patientmanagement/patient_service/kafka/KafkaProducer.java
@@ -1,4 +1,29 @@
 package com.projects.patientmanagement.patient_service.kafka;
 
+
+import com.patientmangement.patientservice.PatientEvent;
+import com.projects.patientmanagement.patient_service.models.PatientEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class KafkaProducer {
+
+
+    private final KafkaTemplate<String,byte[]> kafkaTemplate;
+
+
+
+    public void sendPatientCreatedEvent(PatientEntity patient){
+        PatientEvent patientEvent=PatientEvent
+                .newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .build();
+        kafkaTemplate.send("patient_created_topic",patientEvent.toByteArray());
+    }
+
 }
--- patient-service/src/main/java/com/projects/patientmanagement/patient_service/services/PatientService.java
+++ patient-service/src/main/java/com/projects/patientmanagement/patient_service/services/PatientService.java
@@ -5,6 +5,7 @@ import com.patientmangement.billing.CreateBillingAccountRequest;
 import com.projects.patientmanagement.patient_service.dto.patient.PatientCreationRequestDto;
 import com.projects.patientmanagement.patient_service.dto.patient.PatientResponseDto;
 import com.projects.patientmanagement.patient_service.exceptions.PatientAlreadyRegisteredException;
+import com.projects.patientmanagement.patient_service.kafka.KafkaProducer;
 import com.projects.patientmanagement.patient_service.models.PatientEntity;
 import com.projects.patientmanagement.patient_service.repository.PatientRepository;
 import com.projects.patientmanagement.patient_service.utils.constants.ErrorMessageConstants;
@@ -23,6 +24,7 @@ public class PatientService {
     private final PatientRepository patientRepository;
     private final PatientMapper patientMapper;
     private final BillingServiceGrpc.BillingServiceBlockingStub billingServiceBlockingStub;
+    private final KafkaProducer  kafkaProducer;
 
     public Optional<PatientEntity> getPatientByEmail(String email) {
         return patientRepository.findByEmail(email);
@@ -32,9 +34,10 @@ public class PatientService {
         if (patientRepository.findByEmail(patientCreationRequestDto.getEmail()).isPresent()) {
             throw new PatientAlreadyRegisteredException(ErrorMessageConstants.PATIENT_NOT_FOUND);
         }
-        final var response = billingServiceBlockingStub.createBillingAccount(CreateBillingAccountRequest.newBuilder().setPatientId("1").setEmail("alan").setName("alan2").build());
-        System.out.println("the response is"+response);
-        return patientMapper.fromEntity(patientRepository.save(patientMapper.fromDto(patientCreationRequestDto)));
+         final PatientEntity savedPatient=patientRepository.save(patientMapper.fromDto(patientCreationRequestDto));
+         billingServiceBlockingStub.createBillingAccount(CreateBillingAccountRequest.newBuilder().setPatientId(savedPatient.getId().toString()).setName(savedPatient.getName()).setEmail(savedPatient.getName()).build());
+         kafkaProducer.sendPatientCreatedEvent(savedPatient);
+         return patientMapper.fromEntity(savedPatient);
     }
 
     public List<PatientResponseDto> getAllPatients() {
